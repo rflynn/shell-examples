6a0b685aba35c3a6f3a0fa3e95dc3caaa9072266
Ryan Flynn <parseerror@gmail.com>
2014-06-24 10:42:22 -0400

    wrap lines to 80 cols

--- livecoding.sh
+++ livecoding.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-# You are tasked with writing an application that maps numbers entered on a phone’s dial pad to English words.
-# For example, the number 228 could map to “cat” or “bat” (or any other word composed of letters that have a dial pad value of 228).
+# You are tasked with writing an application that maps numbers entered on a
+# phone’s dial pad to English words.
+# For example, the number 228 could map to “cat” or “bat” (or any other word
+# composed of letters that have a dial pad value of 228).
 #
 # [1     ] [2 abc] [3  def]
 # [4  ghi] [5 jkl] [6  mno]
 # [7 pqrs] [8 tuv] [9 wxyz]
 
-# Your application should have a user interface that allows a user to enter a number and then displays the corresponding word.
+# Your application should have a user interface that allows a user to enter a
+# number and then displays the corresponding word.
 printf "number: "
 read number
 
