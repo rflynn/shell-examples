0fd81093c419f23424457a2202ee808d125e11c4
Ryan Flynn <parseerror@gmail.com>
2014-06-24 18:15:54 -0400

    overlapping is better

--- no-as-a-service.sh
+++ no-as-a-service.sh
@@ -4,9 +4,10 @@
 while [ 1 ]
 do
     nc -w 0 -l 6969;
-    vol=$(osascript -e 'output volume of (get volume settings)');
-    osascript -e "set volume output volume 100";
-    say -v Alex no;
-    osascript -e "set volume output volume $vol";
-    sleep 0.1;
+    (
+    #vol=$(osascript -e 'output volume of (get volume settings)');
+    #osascript -e "set volume output volume 100";
+    say -v Alex no
+    #osascript -e "set volume output volume $vol"
+    ) &
 done
