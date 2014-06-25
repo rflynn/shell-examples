018818f3d9871317f79166e2c69be7739ba69798
Ryan Flynn <parseerror@gmail.com>
2014-06-25 12:47:19 -0400

    add support for multiple paths

--- no-as-a-service.sh
+++ no-as-a-service.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 # "NO" as a service
-# curl http://localhost:6969/
+# curl http://localhost:7879/
 while [ 1 ]
 do
-    echo "HTTP/1.1 200 OK\r\n\r\n" | nc -l 7879;
-    say -v Alex no &
+    path=$(echo "HTTP/1.1 200 OK\r\n\r\n" | nc -l 7879 | head -n 1 | cut -f2 -d' ')
+    case "$path" in
+      "/yes") say=yes ;;
+      "/mo") say=mo ;;
+      "/favicon.ico") say=skip ;;
+      *) say=no ;;
+    esac;
+    [[ "$say" != "skip" ]] && say -v Alex "$say" &
 done
