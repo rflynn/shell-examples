27ee4f3fc68b7ec67bc3d911bf2c8901bb9c660a
Ryan Flynn <parseerror@gmail.com>
2014-06-24 20:55:03 -0400

    oops i suck. this works

--- no-as-a-service.sh
+++ no-as-a-service.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 # "NO" as a service
 # curl http://localhost:6969/
 while [ 1 ]
 do
-    echo -e "HTTP/1.1 200 OK\r\n\r\n" | nc -l 6969
+    echo "HTTP/1.1 200 OK\r\n\r\n" | nc -l 6969;
     say -v Alex no &
 done
